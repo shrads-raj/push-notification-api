60b8f9d405b5493f4b3b4d7491c1b9bc7bfa8080
CodeSandbox <support@codesandbox.io>
2023-02-27 16:40:07 +0000

    "Initial commit"

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    after_action :allow_iframe
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+     # nothing to do here
+  end
+end
